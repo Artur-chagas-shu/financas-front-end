539953d9be2cf8f35ee164efd622db67e14c397c
Artur Chagas <arturchagax@gmail.com>
2026-05-06 02:40:22 -0300

    url fixa

--- js/api.js
+++ js/api.js
@@ -1,4 +1,4 @@
-const API_BASE = 'https://cubic-dale-fotos-dave.trycloudflare.com/api';
+const API_BASE = 'https://api.financasapp.eu.org';
 
 async function apiRequest(endpoint, method, body = null) {
     const token = sessionStorage.getItem('token');
--- js/auth.js
+++ js/auth.js
@@ -1,4 +1,4 @@
-const API_BASE = 'https://cubic-dale-fotos-dave.trycloudflare.com/api';
+const API_BASE = 'https://api.financasapp.eu.org';
 
 const loginForm = document.getElementById('loginForm');
 const registerForm = document.getElementById('registerForm');
